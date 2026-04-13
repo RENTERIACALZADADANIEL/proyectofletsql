099bb15766f0c66a9e96a85cf72b2b6caa79d81b
RENTERIACALZADADANIEL <23308060610259@cetis61.edu.mx>
2026-04-13 17:37:48 -0600

    cambios para que corra

--- src/main.py
+++ src/main.py
@@ -1,24 +1,22 @@
 import flet as ft
-# Importación de Vistas
-from views.LoginView import LoginView
-from views.DashboardView import DashboardView
-# Importación de Controladores
 from controllers.UserController import AuthController
 from controllers.TareaController import TareaController
+from views.LoginView import LoginView
+from views.DashboardView import DashboardView
 
 def start(page: ft.Page):
-    # Inicializamos los controladores
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
 
     def route_change(e):
         page.views.clear()
         
-        # Ruta de Login (Principal)
+        # Caso 1: Login
         if page.route == "/":
+            page.add(ft.Text("Caso 1"))
             page.views.append(LoginView(page, auth_ctrl))
         
-        # Ruta de Dashboard
+        # Caso 2: Dashboard
         elif page.route == "/dashboard":
             page.views.append(DashboardView(page, task_ctrl))
 
@@ -27,10 +25,9 @@ def start(page: ft.Page):
             page.views.append(
                 ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
             )
-        
+            
         page.update()
 
-    # Configuración de la página
     page.on_route_change = route_change
     # Forzamos la navegación inicial
     page.go("/")
