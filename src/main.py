e3550ab1de80dda10836192b758e41b384610007
RENTERIACALZADADANIEL <23308060610259@cetis61.edu.mx>
2026-04-17 09:01:33 -0600

    modificacion de nombres

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import flet as ft 
-from controllers.UserController import AuthController
-from controllers.TareaController import TareaController
+from controllers.usercontroller import AuthController
+from controllers.tareacontroller import TareaController
 from views.LoginView import LoginView
 from views.DashboardView import DashboardView
 
