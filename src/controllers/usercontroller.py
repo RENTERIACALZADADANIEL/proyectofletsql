16a9e38b9f43f0ab77ad9ab68bb91432b25b4e66
RENTERIACALZADADANIEL <23308060610259@cetis61.edu.mx>
2026-04-23 16:49:26 -0600

    codigo que casi funciona

--- src/controllers/usercontroller.py
+++ src/controllers/usercontroller.py
@@ -1,31 +1,16 @@
 class AuthController:
-
     def __init__(self):
-
         pass
 
-   
-
     def login(self, email, password):
-
         # Simulación de BD: las llaves deben coincidir con lo que usa el Dashboard
-
         if email == "admin@gmail.com" and password == "1234":
-
             user = {
-
                 "id_usuario": 1,
-
                 "email": email,
-
                 "nombre": "Administrador",
-
                 "role": "admin"
-
             }
-
             return user, "Login exitoso"
-
         else:
-
             return None, "Correo o contraseña incorrectos"
